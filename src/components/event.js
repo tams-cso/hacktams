710d6f2c56c3eb14228920dd38bc049f79025041
The-Box-Lord <56173725+The-Box-Lord@users.noreply.github.com>
2021-03-14 15:49:35 -0500

    Make Menu and Event components

---
+++ src/components/event.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './event.css';
+
+class Event extends React.Component {
+    render() {
+        return (
+            <div className="event">
+                <div className="event-header">
+                    {header /* TODO have year, dates, # of teams, # of projects */}
+                </div>
+                <div className="event-body">
+                    {/* TODO have devpost link, site link, list of People */}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Event;
