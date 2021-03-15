5da0474a6c83e93bb1ea1ecfea1ab45860104e36
Anirudhx234 <65822278+Anirudhx234@users.noreply.github.com>
2021-03-15 13:36:43 -0500

    Added and fixed footer stuff
    Added footer.js, footer.css, footer_icons.js, footer_icons.css and imported into app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Menu from './components/menu';
 import Event from './components/event';
-
+import Footer from './components/footer';
+import FooterIcons from './components/footer_icons';
 import './app.css';
 
 class App extends React.Component {
@@ -54,31 +55,12 @@ class App extends React.Component {
                         orientated high school hackathon from Denton, TX.
                     </p>
                 </div>
-                
-
                 <div className="events" ref={this.events}>
                     <h1 className="events-title">Events</h1>
 
                 </div>
-                <div className="footer">
-                    <h1 className="contact-us">Contact Us!</h1>
-                    <p className="contact-us-info-box">
-                        Made with &hearts; in Denton, Texas!<br></br>
-                        Contact us at <a href="mailto: team@hacktams.org">team@hacktams.org</a><br></br>
-                        Want to sponsor us? Email us at <a href="mailto: sponsor@hacktams.org">sponsor@hacktams.org</a>
-                    </p>
-                    <div className="footer-item">
-                        <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" class="footer item">
-                            <img src="https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&h=512" /></a>
-                        <a href="https://www.instagram.com/?hl=en" target="_blank" rel="noopener noreferrer" class="footer item">
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1200px-Instagram_logo_2016.svg.png" /></a>
-                        <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" class="footer item">
-                            <img src="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" /></a>
-                        <a href="https://discord.com/" target="_blank" rel="noopener noreferrer" class="footer item">
-                            <img src="https://cdn.iconscout.com/icon/free/png-512/discord-3-569463.png" /></a>
-                    </div>
-                    <a href="https://www.youtube.com/">Check out our youtube channel!</a>
-                </div>
+                <Footer navigate={this.navigate} />
+                <FooterIcons navigate={this.navigate} />
             </div>
         );
     }
