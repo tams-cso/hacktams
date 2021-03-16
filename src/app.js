405774ea6e5c48fe8054fe6ba91197a1d398ec96
Anirudhx234 <65822278+Anirudhx234@users.noreply.github.com>
2021-03-16 13:32:30 -0500

    Anirudh_Testing (#13)
    * Added footer
    
    Logos and text can be changed so this is a rough template. Correct links need to be added.
    
    * Added and fixed footer stuff
    
    Added footer.js, footer.css, footer_icons.js, footer_icons.css and imported into app.js
    
    * Fixed Footer
    
    Footer is fixed, and contact us in the navbar works now. Links for social media should be correct.
    
    * Update app.js
    
    Co-authored-by: DylanLi272 <56173725+DylanLi272@users.noreply.github.com>

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Menu from './components/menu';
-import Event from './components/event';
 import About from './components/about';
+import Home from './components/home';
+import Footer from './components/footer';
 
 import './app.css';
-import Home from './components/home';
 
 class App extends React.Component {
     constructor(props) {
@@ -32,46 +32,10 @@ class App extends React.Component {
                 <Menu navigate={this.navigate} />
                 <Home reference={this.home} />
                 <About reference={this.about} />
-
                 <div className="events" ref={this.events}>
                     <h1 className="events-title">Events</h1>
-                    <Event year="2020" />
                 </div>
-
-                <div className="footer" ref={this.contact}>
-                    <h1 className="contact-us">Contact Us!</h1>
-                    <p className="contact-us-info-box">
-                        Made with &hearts; in Denton, Texas!<br></br>
-                        Contact us at <a href="mailto: team@hacktams.org">team@hacktams.org</a>
-                        <br></br>
-                        Want to sponsor us? Email us at <a href="mailto: sponsor@hacktams.org">sponsor@hacktams.org</a>
-                    </p>
-                    <div className="footer-item">
-                        <a
-                            href="https://www.facebook.com/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            class="footer item"
-                        >
-                            <img src="https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&h=512" />
-                        </a>
-                        <a
-                            href="https://www.instagram.com/?hl=en"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            class="footer item"
-                        >
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1200px-Instagram_logo_2016.svg.png" />
-                        </a>
-                        <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" class="footer item">
-                            <img src="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" />
-                        </a>
-                        <a href="https://discord.com/" target="_blank" rel="noopener noreferrer" class="footer item">
-                            <img src="https://cdn.iconscout.com/icon/free/png-512/discord-3-569463.png" />
-                        </a>
-                    </div>
-                    <a href="https://www.youtube.com/">Check out our youtube channel!</a>
-                </div>
+                <Footer reference={this.contact}/>
             </div>
         );
     }
