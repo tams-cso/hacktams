82e05cea73818691375ed1a34038515891023f17
DylanLi272 <56173725+DylanLi272@users.noreply.github.com>
2021-03-16 12:32:02 -0500

    Make the home and about sections into components (#12)
    * Make the home and about sections into components
    
    * Small update to event
    
    * Formatting fixes
    
    Co-authored-by: The-Box-Lord <56173725+The-Box-Lord@users.noreply.github.com>

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Menu from './components/menu';
 import Event from './components/event';
+import About from './components/about';
 
 import './app.css';
+import Home from './components/home';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { email: '' };
 
         this.home = React.createRef();
         this.about = React.createRef();
@@ -29,53 +30,45 @@ class App extends React.Component {
         return (
             <div className="app">
                 <Menu navigate={this.navigate} />
-                <div className="home" ref={this.home}>
-                    <div className="home-center-container">
-                        <h1 className="home-title">hackTAMS.</h1>
-                        <input
-                            className="home-email-input"
-                            placeholder="Email"
-                            value={this.state.email}
-                            onChange={this.handleEmailChange}
-                        ></input>
-                        <button className="home-submit" onClick={this.submit}>
-                            Sign up for our newsletter!
-                        </button>
-                    </div>
-                </div>
-                <div className="about" ref={this.about}>
-                    <h1 className="about-title">About Us</h1>
-                    <p className="about-info-box">
-                        We organize hackathons! A hackathon is a coding marathon where students come together to create
-                        an innovative project within only 24 hours. Projects can cover topics such as COVID-19,
-                        environment preservation, social good, and improving daily tasks in the form of a website,
-                        mobile app, machine learning models, hardware, and more. Your creativity is your only
-                        limitation! hackTAMS is part of the TAMS Computer Science Organization, and we host a beginner
-                        orientated high school hackathon from Denton, TX.
-                    </p>
-                </div>
-                
+                <Home reference={this.home} />
+                <About reference={this.about} />
 
                 <div className="events" ref={this.events}>
                     <h1 className="events-title">Events</h1>
-                    <Event year='2020' />
+                    <Event year="2020" />
                 </div>
+
                 <div className="footer" ref={this.contact}>
                     <h1 className="contact-us">Contact Us!</h1>
                     <p className="contact-us-info-box">
                         Made with &hearts; in Denton, Texas!<br></br>
-                        Contact us at <a href="mailto: team@hacktams.org">team@hacktams.org</a><br></br>
+                        Contact us at <a href="mailto: team@hacktams.org">team@hacktams.org</a>
+                        <br></br>
                         Want to sponsor us? Email us at <a href="mailto: sponsor@hacktams.org">sponsor@hacktams.org</a>
                     </p>
                     <div className="footer-item">
-                        <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" class="footer item">
-                            <img src="https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&h=512" /></a>
-                        <a href="https://www.instagram.com/?hl=en" target="_blank" rel="noopener noreferrer" class="footer item">
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1200px-Instagram_logo_2016.svg.png" /></a>
+                        <a
+                            href="https://www.facebook.com/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            class="footer item"
+                        >
+                            <img src="https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&h=512" />
+                        </a>
+                        <a
+                            href="https://www.instagram.com/?hl=en"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            class="footer item"
+                        >
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1200px-Instagram_logo_2016.svg.png" />
+                        </a>
                         <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" class="footer item">
-                            <img src="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" /></a>
+                            <img src="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" />
+                        </a>
                         <a href="https://discord.com/" target="_blank" rel="noopener noreferrer" class="footer item">
-                            <img src="https://cdn.iconscout.com/icon/free/png-512/discord-3-569463.png" /></a>
+                            <img src="https://cdn.iconscout.com/icon/free/png-512/discord-3-569463.png" />
+                        </a>
                     </div>
                     <a href="https://www.youtube.com/">Check out our youtube channel!</a>
                 </div>
