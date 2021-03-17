c6ee1bf7f515c25c790836c6ad2d15d51eb5f739
DylanLi272 <56173725+DylanLi272@users.noreply.github.com>
2021-03-16 22:58:20 -0500

    Fix file formatting (#19)
    Co-authored-by: The-Box-Lord <56173725+The-Box-Lord@users.noreply.github.com>
    Co-authored-by: Michael Zhao <37679458+MichaelZhao21@users.noreply.github.com>

--- src/components/footer.js
+++ src/components/footer.js
@@ -13,21 +13,41 @@ class Footer extends React.Component {
                 <h1 className="contact-us">Contact Us!</h1>
                 <p className="contact-us-info-box">
                     Made with &hearts; in Denton, Texas!<br></br>
-                    Contact us at <a href="mailto: team@hacktams.org">team@hacktams.org</a><br></br>
+                    Contact us at <a href="mailto: team@hacktams.org">team@hacktams.org</a>
+                    <br></br>
                     Want to sponsor us? Email us at <a href="mailto: sponsor@hacktams.org">sponsor@hacktams.org</a>
                 </p>
             </div>,
             <div className="footer-item">
-                <a href="https://www.facebook.com/hackTAMS" target="_blank" rel="noopener noreferrer" class="footer item">
-                    <img src={FB} alt="Facebook" /></a>
-                <a href="https://www.instagram.com/hacktams/?hl=en" target="_blank" rel="noopener noreferrer" class="footer item">
-                    <img src={Insta} alt="Instagram" /></a>
+                <a
+                    href="https://www.facebook.com/hackTAMS"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="footer item"
+                >
+                    <img src={FB} alt="Facebook" />
+                </a>
+                <a
+                    href="https://www.instagram.com/hacktams/?hl=en"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="footer item"
+                >
+                    <img src={Insta} alt="Instagram" />
+                </a>
                 <a href="https://twitter.com/thehackTAMS" target="_blank" rel="noopener noreferrer" class="footer item">
-                    <img src={Twitter} alt="Twitter" /></a>
-                <a href="https://www.youtube.com/channel/UCVe1eL6FUcxay8gQ_BtHOjA" target="_blank" rel="noopener noreferrer" class="footer item">
-                    <img src={YouTube} alt="YouTube" /></a>
-            </div>
-        ]
+                    <img src={Twitter} alt="Twitter" />
+                </a>
+                <a
+                    href="https://www.youtube.com/channel/UCVe1eL6FUcxay8gQ_BtHOjA"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="footer item"
+                >
+                    <img src={YouTube} alt="YouTube" />
+                </a>
+            </div>,
+        ];
     }
 }
 
