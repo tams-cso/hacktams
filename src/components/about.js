51390c42c0c2e68129fdf223b52d50e7bb4a3983
Jeongwoo <iamchoij@gmail.com>
2021-04-09 21:04:39 +0000

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -9,7 +9,7 @@ class About extends React.Component {
                 <p className="about-info-box">
                     Our goal is to introduce high school students to real-world computer science
                     skills, tools, and professionals in order to make a positive impact on the community.
-                    hackTAMS is part of the <a href="https://cso.tams.club/" target="_blank">TAMS Computer Science Organization</a>,
+                    hackTAMS is part of the <a href="https://cso.tams.club/" target="_blank" rel="noreferrer">TAMS Computer Science Organization</a>,
                     and we host beginner-orientated high school hackathons in Denton, TX.
                 </p>
             </div>
