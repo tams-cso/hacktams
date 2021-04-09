03766505dc9038cfa206cd356f6769aab3049168
Jeongwoo <iamchoij@gmail.com>
2021-04-09 21:03:17 +0000

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -5,14 +5,12 @@ class About extends React.Component {
     render() {
         return (
             <div className="about" ref={this.props.reference}>
-                <h1 className="about-title">About Us</h1>
+                <h1 className="about-title">We organize hckathons</h1>
                 <p className="about-info-box">
-                    We organize hackathons! A hackathon is a coding marathon where students come together to create an
-                    innovative project within only 24 hours. Projects can cover topics such as COVID-19, environment
-                    preservation, social good, and improving daily tasks in the form of a website, mobile app, machine
-                    learning models, hardware, and more. Your creativity is your only limitation! hackTAMS is part of
-                    the TAMS Computer Science Organization, and we host a beginner orientated high school hackathon from
-                    Denton, TX.
+                    Our goal is to introduce high school students to real-world computer science
+                    skills, tools, and professionals in order to make a positive impact on the community.
+                    hackTAMS is part of the <a href="https://cso.tams.club/" target="_blank">TAMS Computer Science Organization</a>,
+                    and we host beginner-orientated high school hackathons in Denton, TX.
                 </p>
             </div>
         );
