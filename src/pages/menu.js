be867b8258f1a4c7d07bb7cf0384b023c9eb9b66
Michael Zhao <michaelzhao314@gmail.com>
2021-04-19 01:32:19 -0500

    Updated logo and public files

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './menu.css';
 
-import { ReactComponent as Duck } from '../logo-main.svg';
+import { ReactComponent as Duck } from '../images/duck-logo.svg';
 
 class Menu extends React.Component {
     render() {
